c2fa568f8e996adf9262c119588a50fecef4afac
Christian Boxdörfer <christian.boxdoerfer@posteo.de>
2017-02-22 10:06:07 +0100

    initial commit

---
+++ nvim/.config/nvim/init.vim
@@ -0,0 +1,93 @@
+" Specify a directory for plugins (for Neovim: ~/.local/share/nvim/plugged)
+" curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+"    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+call plug#begin('~/.local/share/nvim/plugged')
+
+Plug 'junegunn/vim-easy-align'
+Plug 'ctrlpvim/ctrlp.vim'
+Plug 'altercation/vim-colors-solarized'
+Plug 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
+Plug 'SirVer/ultisnips' | Plug 'honza/vim-snippets'
+Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
+Plug 'Yggdroot/indentLine'
+Plug 'vim-scripts/gtk-vim-syntax'
+Plug 'dag/vim-fish'
+Plug 'vim-scripts/gnuplot-syntax-highlighting'
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'neomake/neomake'
+Plug 'octol/vim-cpp-enhanced-highlight'
+Plug 'arakashic/chromatica.nvim'
+Plug 'airblade/vim-gitgutter'
+Plug 'rust-lang/rust.vim'
+Plug 'timonv/vim-cargo'
+
+"Plug 'majutsushi/tagbar'
+
+" Initialize plugin system
+call plug#end()
+
+set showcmd             " Show (partial) command in status line.
+set showmatch           " Show matching brackets.
+set showmode            " Show current mode.
+set ruler               " Show the line and column numbers of the cursor.
+set number 		        " Show line numbers
+set cursorline		    " Show cursor line
+set formatoptions+=o    " Continue comment marker in new lines.
+set textwidth=0         " Hard-wrap long lines as you type them.
+set expandtab           " Insert spaces when TAB is pressed.
+set tabstop=4           " Render TABs using this many spaces.
+set shiftwidth=4        " Indentation amount for < and > commands.
+
+set noerrorbells        " No beeps.
+set modeline            " Enable modeline.
+set linespace=0         " Set line-spacing to minimum.
+set nojoinspaces        " Prevents inserting two spaces after punctuation on a join (J)
+set autochdir
+set cino=(0
+set cinoptions+=t0
+set cindent
+set ignorecase
+set smartcase
+set undofile
+set mouse=a
+
+let g:airline_theme='solarized'
+let g:airline_powerline_fonts = 1
+
+let g:ctrlp_types = ['mru', 'fil']
+
+" Use deoplete.
+let g:deoplete#enable_at_startup = 1
+
+let g:python_host_prog = '/usr/bin/python2'
+let g:python3_host_prog = '/usr/bin/python3'
+
+" better key bindings for UltiSnipsExpandTrigger
+let g:UltiSnipsExpandTrigger = "<tab>"
+let g:UltiSnipsJumpForwardTrigger = "<tab>"
+let g:UltiSnipsJumpBackwardTrigger = "<s-tab>"
+
+" Colorscheme
+set background=dark
+colorscheme solarized
+
+" Run Neomake on every file save
+autocmd! BufWritePost * Neomake!
+
+let g:neomake_py_flake8_maker = {
+    \ 'args': ['--ignore=E221,E241,E272,E251,W702,E203,E201,E202',  '--format=default'],
+    \ 'errorformat':
+        \ '%E%f:%l: could not compile,%-Z%p^,' .
+        \ '%A%f:%l:%c: %t%n %m,' .
+        \ '%A%f:%l: %t%n %m,' .
+        \ '%-G%.%#',
+    \ }
+let g:neomake_python_enabled_makers = ['flake8']
+
+function! SwitchBuffer()
+  b#
+endfunction
+
+nnoremap <Tab> :call SwitchBuffer()<CR>
+"nnoremap <F8> :TagbarToggle<CR>
---
+++ xmonad/.config/xmonad/xmonad.hs
@@ -0,0 +1,157 @@
+import System.IO
+import System.Exit
+import XMonad
+
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Accordion
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Spacing
+
+import XMonad.Actions.NoBorders
+import XMonad.Actions.WindowGo
+import XMonad.Actions.CycleWS
+
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.Scratchpad
+
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.UrgencyHook
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+
+base03  = "#002b36"
+base02  = "#073642"
+base01  = "#586e75"
+base00  = "#657b83"
+base0   = "#839496"
+base1   = "#93a1a1"
+base2   = "#eee8d5"
+base3   = "#fdf6e3"
+yellow  = "#b58900"
+orange  = "#cb4b16"
+red     = "#dc322f"
+magenta = "#d33682"
+violet  = "#6c71c4"
+blue    = "#268bd2"
+cyan    = "#2aa198"
+green = "#859900"
+
+myLayoutHook = avoidStruts $ smartSpacing 4 $ smartBorders $ (tiled ||| Accordion ||| Full)
+    where
+        tiled = Tall 1 (3/100) (3/5)
+
+myManageHook = scratchpadManageHook (W.RationalRect 0.2 0 0.6 0.5) <+> composeAll
+    [
+        className =? "Firefox" --> doShift "WEB"
+        , className =? "DeaDBeeF" --> doShift "AV"
+        , className =? "Thunderbird" --> doShift "MAIL"
+        , className =? "lxqt-openssh-askpass" --> doFloat
+        , title =? "Python Turtle Graphics" --> doFloat
+        , manageDocks
+    ]
+
+myTerminal    = "gnome-terminal"
+myEditor      = "nvim-wrapper"
+myModMask     = mod4Mask -- Win key or Super_L
+myBorderWidth = 1
+myBorderColorNormal = "#586e75"
+myBorderColorFocused = "#b58900"
+
+myWorkspaces = ["GEN", "WEB", "DEV", "MAIL", "AV"] ++ map show [6..9]
+
+-- | The xmonad key bindings. Add, modify or remove key bindings here.
+--
+-- (The comment formatting character is used when generating the manpage)
+--
+myKeybindings conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+    -- launching and killing programs
+    [ ((modMask,               xK_Return), spawn $ XMonad.terminal conf) -- %! Launch terminal
+    , ((modMask .|. shiftMask, xK_Return), spawn myEditor) -- %! Launch editor
+    , ((modMask,               xK_p     ), spawn "rofi -show drun") -- %! Launch rofi
+    , ((modMask .|. shiftMask, xK_p     ), spawn "rofi -show run") -- %! Launch rofi
+    , ((modMask,               xK_f     ), runOrRaiseMaster "fsearch" (className =? "fsearch")) -- %! Launch fsearch
+    , ((modMask .|. shiftMask, xK_c     ), kill) -- %! Close the focused window
+
+    , ((modMask,               xK_space ), sendMessage NextLayout) -- %! Rotate through the available layout algorithms
+    , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf) -- %!  Reset the layouts on the current workspace to default
+
+    , ((modMask,               xK_n     ), refresh) -- %! Resize viewed windows to the correct size
+
+    -- Scratchpad
+    --, ((modMask,               xK_minus), scratchpadSpawnAction conf)
+    , ((modMask,               xK_minus), scratchpadSpawnActionCustom "st -n scratchpad -e tmux -2 attach")
+
+    -- move focus up or down the window stack
+    , ((modMask,               xK_Tab   ), windows W.focusDown) -- %! Move focus to the next window
+    , ((modMask .|. shiftMask, xK_Tab   ), windows W.focusUp  ) -- %! Move focus to the previous window
+    , ((modMask,               xK_j     ), windows W.focusDown) -- %! Move focus to the next window
+    , ((modMask,               xK_k     ), windows W.focusUp  ) -- %! Move focus to the previous window
+    , ((modMask,               xK_m     ), windows W.focusMaster  ) -- %! Move focus to the master window
+    , ((modMask,               xK_n     ), toggleWS ) -- %! Switch to last focused worksapce
+
+    -- modifying the window order
+    , ((modMask,               xK_s     ), windows W.swapMaster) -- %! Swap the focused window and the master window
+    , ((modMask .|. shiftMask, xK_j     ), windows W.swapDown  ) -- %! Swap the focused window with the next window
+    , ((modMask .|. shiftMask, xK_k     ), windows W.swapUp    ) -- %! Swap the focused window with the previous window
+
+    -- resizing the master/slave ratio
+    , ((modMask,               xK_h     ), sendMessage Shrink) -- %! Shrink the master area
+    , ((modMask,               xK_l     ), sendMessage Expand) -- %! Expand the master area
+
+    -- floating layer support
+    , ((modMask,               xK_t     ), withFocused $ windows . W.sink) -- %! Push window back into tiling
+
+    -- increase or decrease number of windows in the master area
+    , ((modMask              , xK_comma ), sendMessage (IncMasterN 1)) -- %! Increment the number of windows in the master area
+    , ((modMask              , xK_period), sendMessage (IncMasterN (-1))) -- %! Deincrement the number of windows in the master area
+
+    -- quit, or restart
+    , ((modMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess)) -- %! Quit xmonad
+    , ((modMask              , xK_q     ), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi") -- %! Restart xmonad
+    ]
+    ++
+
+        -- mod-[1..9], Switch to workspace N
+    -- mod-shift-[1..9], Move client to workspace N
+    [((m .|. modMask, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+        ++
+
+        -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+        -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+    [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+main = do
+    xmproc <- spawnPipe "xmobar ~/.config/xmobar/config.hs"
+    xmonad $ withUrgencyHook NoUrgencyHook defaultConfig
+        { terminal    = myTerminal
+        , modMask     = myModMask
+        , borderWidth = myBorderWidth
+        , normalBorderColor = myBorderColorNormal
+        , focusedBorderColor = myBorderColorFocused
+        , workspaces  = myWorkspaces
+        --, startupHook = myStartupHook
+        , keys = myKeybindings
+        , handleEventHook = mconcat [
+            docksEventHook,
+            handleEventHook defaultConfig
+        ]
+        , layoutHook  = myLayoutHook
+        , manageHook  = myManageHook <+> manageHook defaultConfig
+        , logHook     = dynamicLogWithPP $ xmobarPP {
+            ppOutput = hPutStrLn xmproc
+                , ppTitle = xmobarColor blue "" . shorten 50
+                , ppCurrent = xmobarColor "#c0c0c0" "" . wrap "" ""
+                , ppSep = xmobarColor red "" "  :  "
+                , ppUrgent = xmobarColor "#dc322f" ""
+                --, ppLayout = xmobarColor yellow ""
+                , ppLayout = const ""
+        }
+        }
+
+
